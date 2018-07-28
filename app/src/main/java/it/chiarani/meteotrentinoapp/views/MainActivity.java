1b1428201946914bf5dbc3a383485676284b788f
Fabio Chiarani <fabio@chiarani.it>
2018-07-28 16:49:26 +0200

    [REF] move activities into views package

--- app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
@@ -1,7 +1,9 @@
-package it.chiarani.meteotrentinoapp;
+package it.chiarani.meteotrentinoapp.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import it.chiarani.meteotrentinoapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
