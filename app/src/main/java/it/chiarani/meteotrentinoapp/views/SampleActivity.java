1f5a7a767eb777dea8c45c2cb9b2a38094be94e8
Fabio Chiarani <fabio@chiarani.it>
2018-07-28 17:05:38 +0200

    [UP] sample activity methods

--- app/src/main/java/it/chiarani/meteotrentinoapp/views/SampleActivity.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/views/SampleActivity.java
@@ -1,4 +1,41 @@
+/**
+ * @author Fabio Chiarani
+ * @version 1
+ */
+
 package it.chiarani.meteotrentinoapp.views;
 
-public class SampleActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Sample activity to extends
+ */
+public abstract class SampleActivity extends AppCompatActivity{
+
+  /**
+   * Get activity layout ID
+   * @return activity layout ID
+   */
+  protected abstract int getLayoutID();
+
+  /**
+   * Set activity binding with UI
+   */
+  protected abstract void setActivityBinding();
+
+  /**
+   * Override onCreate method
+   * @param savedInstanceState bundle
+   */
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Why? An Activity may not have a layout. If that is the case, layoutId is zero
+    if (getLayoutID() == 0) {
+      return;
+    }
+
+    setActivityBinding();
+  }
 }
