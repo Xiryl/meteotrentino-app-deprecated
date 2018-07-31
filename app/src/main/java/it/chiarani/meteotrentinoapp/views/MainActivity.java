5b450a00fda6feeaa476665e0b3ab50bb26a80ee
Fabio Chiarani <fabio@chiarani.it>
2018-07-31 12:56:37 +0200

    [REF] mainactivity now use databinding & extend sampleactivity

--- app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
@@ -1,15 +1,28 @@
 package it.chiarani.meteotrentinoapp.views;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-
 import it.chiarani.meteotrentinoapp.R;
+import it.chiarani.meteotrentinoapp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SampleActivity {
+
+  // Private Fields
+  private ActivityMainBinding binding;
+  // End
+
+  @Override
+  protected int getLayoutID() {
+    return R.layout.activity_main;
+  }
+
+  @Override
+  protected void setActivityBinding() {
+    binding = DataBindingUtil.setContentView(this, getLayoutID());
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
   }
 }
