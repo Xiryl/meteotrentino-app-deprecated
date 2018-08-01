022789b376970d37a843899e7055888fb5097f14
Fabio Chiarani <fabio@chiarani.it>
2018-08-01 16:47:25 +0200

    [UP] rv with layout (tested)

--- app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherSlotAdapter.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherSlotAdapter.java
@@ -1,37 +1,52 @@
 package it.chiarani.meteotrentinoapp.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import it.chiarani.meteotrentinoapp.R;
 
 public class WeatherSlotAdapter extends RecyclerView.Adapter<WeatherSlotAdapter.ViewHolder> {
 
-  public WeatherSlotAdapter() {
+  String[] _data;
 
+  public WeatherSlotAdapter(String[] data) {
+    _data = data;
   }
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
 
+    TextView txt_time_slot;
+
     public ViewHolder(View v) {
       super(v);
+      txt_time_slot = v.findViewById(R.id.item_slot_weather_txt_fascia);
     }
   }
 
   @Override
   public WeatherSlotAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    //Define a layout file for individual list item
-    return null;
+
+    Boolean attachViewImmediatelyToParent = false;
+
+    View singleItemLayout = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_slot_weather, parent, attachViewImmediatelyToParent);
+    ViewHolder myViewHolder = new ViewHolder(singleItemLayout);
+
+    return myViewHolder;
   }
 
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
     //Set data to the individual list item
+    holder.txt_time_slot.setText(_data[position]);
   }
 
   @Override
   public int getItemCount() {
     //Return the number of items in your list
-    return 0;
+    return _data.length;
   }
-  
+
 }
--- app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
@@ -2,13 +2,18 @@ package it.chiarani.meteotrentinoapp.views;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import it.chiarani.meteotrentinoapp.R;
+import it.chiarani.meteotrentinoapp.adapters.WeatherSlotAdapter;
 import it.chiarani.meteotrentinoapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends SampleActivity {
 
   // Private Fields
   private ActivityMainBinding binding;
+  private RecyclerView mRecyclerView;
   // End
 
   @Override
@@ -24,5 +29,19 @@ public class MainActivity extends SampleActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_rv_weather_slot);
+
+    // use this setting to improve performance if you know that changes
+    // in content do not change the layout size of the RecyclerView
+    mRecyclerView.setHasFixedSize(true);
+
+    LinearLayoutManager horizontalLayoutManagaer = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+    mRecyclerView.setLayoutManager(horizontalLayoutManagaer);
+
+    // specify an adapter (see also next example)
+    String[] tmp = {"Fascia 0-6", "Fascia 6-12", "Fascia 12-18", "Fascia 18-24"};
+    WeatherSlotAdapter adapter = new WeatherSlotAdapter(tmp);
+    mRecyclerView.setAdapter(adapter);
   }
 }
