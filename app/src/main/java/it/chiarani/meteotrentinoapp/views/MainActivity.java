e9ee385062098fa3e231eb4dc785be23c70638ef
Fabio Chiarani <fabio@chiarani.it>
2018-08-06 16:21:57 +0200

    [REF] mainactivity use binding & add async task callback

--- app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/views/MainActivity.java
@@ -1,20 +1,30 @@
 package it.chiarani.meteotrentinoapp.views;
 
+import android.app.AlertDialog;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
 
 import it.chiarani.meteotrentinoapp.R;
 import it.chiarani.meteotrentinoapp.adapters.WeatherSlotAdapter;
+import it.chiarani.meteotrentinoapp.api.API_locality;
+import it.chiarani.meteotrentinoapp.api.API_locality_response;
 import it.chiarani.meteotrentinoapp.databinding.ActivityMainBinding;
+import it.chiarani.meteotrentinoapp.models.Locality;
 
-public class MainActivity extends SampleActivity {
+public class MainActivity extends SampleActivity implements API_locality_response{
 
-  // Private Fields
-  private ActivityMainBinding binding;
-  private RecyclerView mRecyclerView;
-  // End
+  // #REGION PRIVATE FIELDS
+  private final static String MAINACTIVITY_TAG = "MAINACTIVITY";
+  ActivityMainBinding binding;
+  public ArrayList<Locality> mylocs;
+  // #ENDREGION
 
   @Override
   protected int getLayoutID() {
@@ -30,18 +40,36 @@ public class MainActivity extends SampleActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_rv_weather_slot);
+    Log.d( MAINACTIVITY_TAG, "Start mainactivity");
+
+    // Launch asynctask for get locality
+    new API_locality(this, this::processFinish).execute();
 
     // use this setting to improve performance if you know that changes
     // in content do not change the layout size of the RecyclerView
-    mRecyclerView.setHasFixedSize(true);
+    binding.activityMainRvWeatherSlot.setHasFixedSize(true);
 
     LinearLayoutManager horizontalLayoutManagaer = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
-    mRecyclerView.setLayoutManager(horizontalLayoutManagaer);
+    binding.activityMainRvWeatherSlot.setLayoutManager(horizontalLayoutManagaer);
 
     // specify an adapter (see also next example)
     String[] tmp = {" Fascia oraria: 0-6 ", " Fascia oraria: 6-12 ", " Fascia oraria: 12-18 ", " Fascia oraria: 8-24 "};
     WeatherSlotAdapter adapter = new WeatherSlotAdapter(tmp);
-    mRecyclerView.setAdapter(adapter);
+    binding.activityMainRvWeatherSlot.setAdapter(adapter);
+
+/*
+    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+    LayoutInflater inflater = this.getLayoutInflater();
+    View dialogView = inflater.inflate(R.layout.today_weather_dialog, null);
+    dialogBuilder.setView(dialogView);
+
+    AlertDialog alertDialog = dialogBuilder.create();
+    alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    alertDialog.show();*/
+  }
+
+  @Override
+  public void processFinish(ArrayList<Locality> output) {
+    mylocs = output;
   }
 }
