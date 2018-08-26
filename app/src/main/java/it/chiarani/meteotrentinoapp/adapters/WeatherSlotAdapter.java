9fd87e0251ddde412afd0e39b224471ddafc4372
Fabio Chiarani <fabio@chiarani.it>
2018-08-26 10:40:06 +0200

    [REF] RecyclerView adapter for weather data

--- app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherSlotAdapter.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherSlotAdapter.java
@@ -7,22 +7,31 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import it.chiarani.meteotrentinoapp.R;
+import it.chiarani.meteotrentinoapp.models.WeatherReport;
 
 public class WeatherSlotAdapter extends RecyclerView.Adapter<WeatherSlotAdapter.ViewHolder> {
 
-  String[] _data;
+  WeatherReport _report;
 
-  public WeatherSlotAdapter(String[] data) {
-    _data = data;
+  public WeatherSlotAdapter(WeatherReport report) {
+    _report = report;
   }
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
 
     TextView txt_time_slot;
+    TextView txt_humidity;
+    TextView txt_prob_prec;
+    TextView txt_prob_temp;
+    TextView txt_vento;
 
     public ViewHolder(View v) {
       super(v);
       txt_time_slot = v.findViewById(R.id.item_slot_weather_txt_fascia);
+      txt_humidity = v.findViewById(R.id.item_slot_weather_txt_humidity);
+      txt_prob_prec = v.findViewById(R.id.item_slot_weather_txt_prob_prec);
+      txt_prob_temp = v.findViewById(R.id.item_slot_weather_txt_prob_temp);
+      txt_vento = v.findViewById(R.id.item_slot_weather_txt_vento);
     }
   }
 
@@ -40,13 +49,17 @@ public class WeatherSlotAdapter extends RecyclerView.Adapter<WeatherSlotAdapter.
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
     //Set data to the individual list item
-    holder.txt_time_slot.setText(_data[position]);
+    holder.txt_time_slot.setText(_report.getPrevisione().getGiorni().get(0).getFasce().get(position).getFasciaOre());
+    holder.txt_prob_prec.setText(_report.getPrevisione().getGiorni().get(0).getFasce().get(position).getDescPrecProb());
+    holder.txt_humidity.setText("75%");
+    holder.txt_prob_temp.setText(_report.getPrevisione().getGiorni().get(0).getFasce().get(position).getDescTempProb());
+    holder.txt_vento.setText(_report.getPrevisione().getGiorni().get(0).getFasce().get(position).getDescVentoIntValle());
   }
 
   @Override
   public int getItemCount() {
     //Return the number of items in your list
-    return _data.length;
+    return _report.getPrevisione().getGiorni().get(0).getFasce().size();
   }
 
 }
