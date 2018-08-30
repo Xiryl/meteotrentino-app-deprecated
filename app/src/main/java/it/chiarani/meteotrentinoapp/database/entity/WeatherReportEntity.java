78287fc21ad7c4d2d3421cdc757475fc9612b0e3
Fabio Chiarani <fabio@chiarani.it>
2018-08-30 14:25:10 +0200

    [ADD] Entity - WeatherReport

---
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/entity/WeatherReportEntity.java
@@ -0,0 +1,85 @@
+package it.chiarani.meteotrentinoapp.database.entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import it.chiarani.meteotrentinoapp.models.WeatherForWeek;
+
+public class WeatherReportEntity {
+
+  // #REGION PRIVATE FIELDS
+  private String dataPubblicazione;
+
+  @PrimaryKey
+  private int idPrevisione;
+  private String evoluzione;
+  private String evoluzioneBreve;
+  private String[] allerteList;
+  private WeatherForWeek previsione;
+  // #ENDREGION
+
+  /**
+   * def. constructor
+   */
+  public WeatherReportEntity() {
+
+  }
+
+  /**
+   * full constructor
+   */
+  public WeatherReportEntity(String dataPubblicazione, int idPrevisione, String evoluzione, String evoluzioneBreve, String[] allerteList, WeatherForWeek previsione) {
+    this.dataPubblicazione = dataPubblicazione;
+    this.idPrevisione = idPrevisione;
+    this.evoluzione = evoluzione;
+    this.evoluzioneBreve = evoluzioneBreve;
+    this.allerteList = allerteList;
+    this.previsione = previsione;
+  }
+
+  public String getDataPubblicazione() {
+    return dataPubblicazione;
+  }
+
+  public void setDataPubblicazione(String dataPubblicazione) {
+    this.dataPubblicazione = dataPubblicazione;
+  }
+
+  public int getIdPrevisione() {
+    return idPrevisione;
+  }
+
+  public void setIdPrevisione(int idPrevisione) {
+    this.idPrevisione = idPrevisione;
+  }
+
+  public String getEvoluzione() {
+    return evoluzione;
+  }
+
+  public void setEvoluzione(String evoluzione) {
+    this.evoluzione = evoluzione;
+  }
+
+  public String getEvoluzioneBreve() {
+    return evoluzioneBreve;
+  }
+
+  public void setEvoluzioneBreve(String evoluzioneBreve) {
+    this.evoluzioneBreve = evoluzioneBreve;
+  }
+
+  public String[] getAllerteList() {
+    return allerteList;
+  }
+
+  public void setAllerteList(String[] allerteList) {
+    this.allerteList = allerteList;
+  }
+
+  public WeatherForWeek getPrevisione() {
+    return previsione;
+  }
+
+  public void setPrevisione(WeatherForWeek previsione) {
+    this.previsione = previsione;
+  }
+}
