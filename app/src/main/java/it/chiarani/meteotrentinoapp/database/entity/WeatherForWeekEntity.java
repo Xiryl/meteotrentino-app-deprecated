842f515d0330b6241b99ed8295b635d283183241
Fabio Chiarani <fabio@chiarani.it>
2018-08-30 14:24:17 +0200

    [ADD] Entity - WeatherForWeek

---
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/entity/WeatherForWeekEntity.java
@@ -0,0 +1,55 @@
+package it.chiarani.meteotrentinoapp.database.entity;
+import android.arch.persistence.room.Entity;
+
+import java.util.List;
+import it.chiarani.meteotrentinoapp.models.WeatherForDay;
+
+@Entity
+public class WeatherForWeekEntity {
+
+  // #REGION PRIVATE FIELDS
+  private String localita;
+  private int quota;
+  private List<WeatherForDay> giorni;
+  // #ENDREGION
+
+  /**
+   * def. constructor
+   */
+  public WeatherForWeekEntity() {
+
+  }
+
+  /**
+   * full constructor
+   */
+  public WeatherForWeekEntity(String localita, int quota, List<WeatherForDay> giorni) {
+    this.localita = localita;
+    this.quota = quota;
+    this.giorni = giorni;
+  }
+
+  public String getLocalita() {
+    return localita;
+  }
+
+  public void setLocalita(String localita) {
+    this.localita = localita;
+  }
+
+  public int getQuota() {
+    return quota;
+  }
+
+  public void setQuota(int quota) {
+    this.quota = quota;
+  }
+
+  public List<WeatherForDay> getGiorni() {
+    return giorni;
+  }
+
+  public void setGiorni(List<WeatherForDay> giorni) {
+    this.giorni = giorni;
+  }
+}
