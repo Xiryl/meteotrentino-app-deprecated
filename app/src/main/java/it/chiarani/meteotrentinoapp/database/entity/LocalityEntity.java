2fa867acc008ed8525828be6759376f4f9bab491
Fabio Chiarani <fabio@chiarani.it>
2018-08-30 14:20:11 +0200

    [ADD] Entity - Locality

---
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/entity/LocalityEntity.java
@@ -0,0 +1,78 @@
+package it.chiarani.meteotrentinoapp.database.entity;
+
+import android.arch.persistence.room.Entity;
+
+@Entity
+public class LocalityEntity {
+
+  // #REGION PRIVATE FIELDS
+  private String loc;
+  private String municipality;
+  private int    height;
+  private String latitude;
+  private String longitude;
+  // #ENDREGION
+
+
+  /**
+   * def. constructor
+   */
+  public LocalityEntity() {
+
+  }
+
+  /**
+   * full constructor
+   */
+  public LocalityEntity(String loc, String municipality, int height, String latitude, String longitude) {
+    this.loc          = loc;
+    this.municipality = municipality;
+    this.height       = height;
+    this.latitude     = latitude;
+    this.longitude    = longitude;
+  }
+
+  // #REGION GETTER & SETTER
+
+  public String getLoc() {
+    return loc;
+  }
+
+  public void setLoc(String loc) {
+    this.loc = loc;
+  }
+
+  public String getMunicipality() {
+    return municipality;
+  }
+
+  public void setMunicipality(String municipality) {
+    this.municipality = municipality;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  // #ENDREGION
+}
