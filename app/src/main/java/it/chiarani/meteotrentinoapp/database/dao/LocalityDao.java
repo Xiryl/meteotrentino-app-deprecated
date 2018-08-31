7c519f83d3293678057fa67ec72df6f8498390e9
Fabio Chiarani <fabio@chiarani.it>
2018-08-31 20:46:47 +0200

    [CLEAN CODE]

--- app/src/main/java/it/chiarani/meteotrentinoapp/database/dao/LocalityDao.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/dao/LocalityDao.java
@@ -2,14 +2,10 @@ package it.chiarani.meteotrentinoapp.database.dao;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Index;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
-
 import java.util.List;
-
 import it.chiarani.meteotrentinoapp.database.entity.LocalityEntity;
-
 
 @Dao
 public interface LocalityDao {
