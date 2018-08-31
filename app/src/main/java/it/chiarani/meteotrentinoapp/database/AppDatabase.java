d48b2cfbf950a3327d5e6686225903271a8b4c0f
Fabio Chiarani <fabio@chiarani.it>
2018-08-31 20:09:35 +0200

    [ADD] Database class

---
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/AppDatabase.java
@@ -0,0 +1,12 @@
+package it.chiarani.meteotrentinoapp.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+import it.chiarani.meteotrentinoapp.database.dao.LocalityDao;
+import it.chiarani.meteotrentinoapp.database.entity.LocalityEntity;
+
+@Database(entities = {LocalityEntity.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+  public abstract LocalityDao localityDao();
+}
