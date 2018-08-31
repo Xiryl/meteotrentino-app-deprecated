3a799fead34160d664d86069a67d9efdab475a13
Fabio Chiarani <fabio@chiarani.it>
2018-08-31 21:09:33 +0200

    [UP] add weatherreportdao

--- app/src/main/java/it/chiarani/meteotrentinoapp/database/AppDatabase.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/AppDatabase.java
@@ -4,9 +4,11 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import it.chiarani.meteotrentinoapp.database.dao.LocalityDao;
+import it.chiarani.meteotrentinoapp.database.dao.WeatherReportDao;
 import it.chiarani.meteotrentinoapp.database.entity.LocalityEntity;
 
 @Database(entities = {LocalityEntity.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
   public abstract LocalityDao localityDao();
+  public abstract WeatherReportDao weatherReportDao();
 }
