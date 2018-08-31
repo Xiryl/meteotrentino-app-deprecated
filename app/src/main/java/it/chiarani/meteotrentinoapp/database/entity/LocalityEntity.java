da207add97a5acdfdba1418111e270c520721ae2
Fabio Chiarani <fabio@chiarani.it>
2018-08-31 20:35:40 +0200

    [REF] implement LocalityEntity from interface

--- app/src/main/java/it/chiarani/meteotrentinoapp/database/entity/LocalityEntity.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/entity/LocalityEntity.java
@@ -3,12 +3,14 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 
+import it.chiarani.meteotrentinoapp.models.Locality;
+
 /**
  * Guide: https://developer.android.com/training/data-storage/room/
  */
 
 @Entity(tableName = "locality")
-public class LocalityEntity {
+public class LocalityEntity implements Locality {
 
   // #REGION PRIVATE FIELDS
   @PrimaryKey (autoGenerate = true)
@@ -43,50 +45,62 @@ public class LocalityEntity {
 
   // #REGION GETTER & SETTER
 
+  @Override
   public String getLoc() {
     return loc;
   }
 
+  @Override
   public void setLoc(String loc) {
     this.loc = loc;
   }
 
+  @Override
   public String getMunicipality() {
     return municipality;
   }
 
+  @Override
   public void setMunicipality(String municipality) {
     this.municipality = municipality;
   }
 
+  @Override
   public int getHeight() {
     return height;
   }
 
+  @Override
   public void setHeight(int height) {
     this.height = height;
   }
 
+  @Override
   public String getLatitude() {
     return latitude;
   }
 
+  @Override
   public void setLatitude(String latitude) {
     this.latitude = latitude;
   }
 
+  @Override
   public String getLongitude() {
     return longitude;
   }
 
+  @Override
   public void setLongitude(String longitude) {
     this.longitude = longitude;
   }
 
+  @Override
   public int getIdLocality() {
     return idLocality;
   }
 
+  @Override
   public void setIdLocality(int idLocality) {
     this.idLocality = idLocality;
   }
