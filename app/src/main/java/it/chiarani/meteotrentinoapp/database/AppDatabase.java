a2c561680e47babe5732cd1be18c63ebcb8cf582
Fabio Chiarani <fabio@chiarani.it>
2018-09-01 07:33:20 +0200

    [UP] AppDatabase with converters

--- app/src/main/java/it/chiarani/meteotrentinoapp/database/AppDatabase.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/database/AppDatabase.java
@@ -2,12 +2,16 @@ package it.chiarani.meteotrentinoapp.database;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 
+import it.chiarani.meteotrentinoapp.database.converters.WeatherForWeekConverter;
 import it.chiarani.meteotrentinoapp.database.dao.LocalityDao;
 import it.chiarani.meteotrentinoapp.database.dao.WeatherReportDao;
 import it.chiarani.meteotrentinoapp.database.entity.LocalityEntity;
+import it.chiarani.meteotrentinoapp.database.entity.WeatherReportEntity;
 
-@Database(entities = {LocalityEntity.class}, version = 1, exportSchema = false)
+@Database(entities = {LocalityEntity.class, WeatherReportEntity.class}, version = 1, exportSchema = false)
+@TypeConverters({WeatherForWeekConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
   public abstract LocalityDao localityDao();
   public abstract WeatherReportDao weatherReportDao();
