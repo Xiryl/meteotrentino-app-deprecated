3f7643c2f4dabad2d8201fcd5838706f989968c2
Fabio Chiarani <fabio@chiarani.it>
2018-09-03 15:33:17 +0200

    [UP] WeatherReportAdapter

--- app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherReportAdapter.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherReportAdapter.java
@@ -9,7 +9,9 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import it.chiarani.meteotrentinoapp.R;
+import it.chiarani.meteotrentinoapp.database.entity.WeatherForSlotEntity;
 import it.chiarani.meteotrentinoapp.database.entity.WeatherReportEntity;
+import it.chiarani.meteotrentinoapp.models.WeatherForSlot;
 import it.chiarani.meteotrentinoapp.models.WeatherReport;
 
 public class WeatherReportAdapter extends RecyclerView.Adapter<WeatherReportAdapter.ViewHolder> {
@@ -38,19 +40,19 @@ public class WeatherReportAdapter extends RecyclerView.Adapter<WeatherReportAdap
 
     public ViewHolder(View v) {
       super(v);
-      txt_time_slot = v.findViewById(R.id.item_weather_report_txt_fascia);
-      txt_tmin = v.findViewById(R.id.item_weather_report_txt_t_min);
-      txt_tmax = v.findViewById(R.id.item_weather_report_txt_t_max);
-      txt_humidity = v.findViewById(R.id.item_weather_report_txt_humidity);
-      txt_pressure = v.findViewById(R.id.item_weather_report_txt_pressure);
-      txt_prob_prec = v.findViewById(R.id.item_weather_report_txt_prob_prec);
-      txt_intensita_prec = v.findViewById(R.id.item_weather_report_txt_intensita_prec);
-      txt_prob_temp = v.findViewById(R.id.item_weather_report_txt_prob_temp);
-      txt_vento_quota = v.findViewById(R.id.item_weather_report_txt_vento_quota);
-      txt_vento_valle = v.findViewById(R.id.item_weather_report_txt_vento_valle);
-      txt_vento_quota_dir = v.findViewById(R.id.item_weather_report_txt_vento_quota_dir);
-      txt_vento_valle_dir = v.findViewById(R.id.item_weather_report_txt_vento_valle_dir);
-      txt_zero_termico = v.findViewById(R.id.item_weather_report_txt_zero_termico);
+      txt_time_slot         = v.findViewById(R.id.item_weather_report_txt_fascia);
+      txt_tmin              = v.findViewById(R.id.item_weather_report_txt_t_min);
+      txt_tmax              = v.findViewById(R.id.item_weather_report_txt_t_max);
+      txt_humidity          = v.findViewById(R.id.item_weather_report_txt_humidity);
+      txt_pressure          = v.findViewById(R.id.item_weather_report_txt_pressure);
+      txt_prob_prec         = v.findViewById(R.id.item_weather_report_txt_prob_prec);
+      txt_intensita_prec    = v.findViewById(R.id.item_weather_report_txt_intensita_prec);
+      txt_prob_temp         = v.findViewById(R.id.item_weather_report_txt_prob_temp);
+      txt_vento_quota       = v.findViewById(R.id.item_weather_report_txt_vento_quota);
+      txt_vento_valle       = v.findViewById(R.id.item_weather_report_txt_vento_valle);
+      txt_vento_quota_dir   = v.findViewById(R.id.item_weather_report_txt_vento_quota_dir);
+      txt_vento_valle_dir   = v.findViewById(R.id.item_weather_report_txt_vento_valle_dir);
+      txt_zero_termico      = v.findViewById(R.id.item_weather_report_txt_zero_termico);
     }
   }
 
@@ -68,7 +70,21 @@ public class WeatherReportAdapter extends RecyclerView.Adapter<WeatherReportAdap
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
     //Set data to the individual list item
-    holder.txt_time_slot.setText("pos" + position);
+    WeatherForSlotEntity wfs = _report.getPrevisione().getGiorni().get(0).getFasce().get(position);
+
+    holder.txt_time_slot.setText(wfs.getFasciaPer() + ": " + wfs.getFasciaOre());
+    holder.txt_tmin.setText("Temperatura minima:" + _report.getPrevisione().getGiorni().get(0).gettMinGiorno() + "°");
+    holder.txt_tmax.setText("Temperatura massima:" + _report.getPrevisione().getGiorni().get(0).gettMaxGiorno() + "°");
+    holder.txt_humidity.setText("Umidità:" + "99 %");
+    holder.txt_pressure.setText("Pressione:" +"99 hPa");
+    holder.txt_prob_prec.setText("Probabilità precipitazioni:" +wfs.getDescPrecProb() + "");
+    holder.txt_intensita_prec.setText("Intensità precipitazioni:" +wfs.getDescPrecInten() + "");
+    holder.txt_prob_temp.setText("Probabilità Temporali:" +wfs.getDescTempProb() + "");
+    holder.txt_vento_quota.setText("Intensità vento in quota:" +wfs.getDescVentoIntQuota() + "");
+    holder.txt_vento_valle.setText("Intensità vento in valle:" +wfs.getDescVentoIntValle() + "");
+    holder.txt_vento_quota_dir.setText("Direzione vento in quota:" +wfs.getDescVentoIntQuota() + "");
+    holder.txt_vento_valle_dir.setText("Direzione vento in valle:" +wfs.getDescVentoIntValle() + "");
+    holder.txt_zero_termico.setText("Zero termico:" +wfs.getZeroTermico() + "");
   }
 
   @Override
