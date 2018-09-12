97bb4019a001cff64ac6361c4e02d7a6ff39a294
Fabio Chiarani <fabio@chiarani.it>
2018-09-12 10:24:06 +0200

    [ADD] radar fragment

---
+++ app/src/main/java/it/chiarani/meteotrentinoapp/fragments/RadarFragment.java
@@ -0,0 +1,63 @@
+package it.chiarani.meteotrentinoapp.fragments;
+
+  import android.databinding.DataBindingUtil;
+  import android.os.Bundle;
+  import android.support.annotation.NonNull;
+  import android.support.annotation.Nullable;
+  import android.support.v4.app.Fragment;
+  import android.support.v4.widget.DrawerLayout;
+  import android.support.v7.widget.LinearLayoutManager;
+  import android.view.Gravity;
+  import android.view.LayoutInflater;
+  import android.view.View;
+  import android.view.ViewGroup;
+  import android.widget.ImageButton;
+
+  import com.bumptech.glide.Glide;
+  import com.bumptech.glide.load.engine.DiskCacheStrategy;
+  import com.bumptech.glide.request.RequestOptions;
+
+  import it.chiarani.meteotrentinoapp.R;
+  import it.chiarani.meteotrentinoapp.adapters.WeatherSevenDayAdapter;
+  import it.chiarani.meteotrentinoapp.api.API_weatherReport;
+  import it.chiarani.meteotrentinoapp.api.API_weatherReport_response;
+  import it.chiarani.meteotrentinoapp.database.entity.WeatherReportEntity;
+  import it.chiarani.meteotrentinoapp.databinding.ActivityMainBinding;
+  import it.chiarani.meteotrentinoapp.databinding.FragmentRadarBinding;
+  import it.chiarani.meteotrentinoapp.databinding.FragmentSevenDayBinding;
+  import it.chiarani.meteotrentinoapp.repositories.WeatherReportRepository;
+
+public class RadarFragment extends Fragment {
+
+  FragmentRadarBinding binding;
+  ActivityMainBinding binging_act;
+
+  public RadarFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_radar, container, false);
+    binging_act  = DataBindingUtil.inflate(inflater, R.layout.activity_main, container, false);
+
+
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    Glide.with(this)
+        .load("https://content.meteotrentino.it/dati-meteo/radar/home/lastRadar4mobile.aspx")
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+                .into(binding.fragmentRadarImgRadarNordItaly);
+  }
+
+}
