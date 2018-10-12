92d0a3cab39c7179f0fd1e2296b8c0f0f0fc1600
Fabio Chiarani <fabio@chiarani.it>
2018-10-12 17:16:46 +0200

    [UP] Clean Code - WeatherReportAdapter

--- app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherReportAdapter.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/adapters/WeatherReportAdapter.java
@@ -1,8 +1,7 @@
 package it.chiarani.meteotrentinoapp.adapters;
 
-import android.arch.persistence.room.util.StringUtil;
+import android.content.Context;
 import android.graphics.Color;
-import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,19 +12,23 @@ import android.widget.TextView;
 
 import it.chiarani.meteotrentinoapp.R;
 import it.chiarani.meteotrentinoapp.database.entity.OpenWeatherDataEntity;
+import it.chiarani.meteotrentinoapp.database.entity.WeatherForDayEntity;
 import it.chiarani.meteotrentinoapp.database.entity.WeatherForSlotEntity;
 import it.chiarani.meteotrentinoapp.database.entity.WeatherReportEntity;
+import it.chiarani.meteotrentinoapp.models.WeatherForDay;
+import it.chiarani.meteotrentinoapp.models.WeatherForSlot;
 
 /**
- * Adapter for weather dayly detail Recyclerview
+ * Adapter for weather detail used in BulletinActivity
  */
 public class WeatherReportAdapter extends RecyclerView.Adapter<WeatherReportAdapter.ViewHolder> {
 
   // #region private fields
-  private WeatherReportEntity weather_report;
+  private WeatherReportEntity   weather_report;
   private OpenWeatherDataEntity open_weather_report;
-  private int weather_day;
-  private int mExpandedPosition = -1;
+  private int                   weather_day;
+  private int                   mExpandedPosition = -1;
+  private Context               mContext;
   // #endregion
 
   /**
@@ -34,54 +37,55 @@ public class WeatherReportAdapter extends RecyclerView.Adapter<WeatherReportAdap
    * @param open_weather_report openweather report data
    * @param weather_day day number (1-7)
    */
-  public WeatherReportAdapter(WeatherReportEntity weather_report, OpenWeatherDataEntity open_weather_report, int weather_day) {
-    this.weather_report = weather_report;
+  public WeatherReportAdapter(Context mContext, WeatherReportEntity weather_report, OpenWeatherDataEntity open_weather_report, int weather_day) {
+    this.mContext            = mContext;
+    this.weather_report      = weather_report;
     this.open_weather_report = open_weather_report;
-    this.weather_day = weather_day;
+    this.weather_day         = weather_day;
   }
 
 
   public static class ViewHolder extends RecyclerView.ViewHolder {
 
-    TextView txt_time_slot;
-    TextView txt_tmin;
-    TextView txt_tmax;
-    TextView txt_pressure;
-    TextView txt_prob_prec;
-    TextView txt_intensita_prec;
-    TextView txt_prob_temp;
-    TextView txt_vento_quota;
-    TextView txt_vento_valle;
-    TextView txt_vento_quota_dir;
-    TextView txt_vento_valle_dir;
-    TextView txt_zero_termico;
-    TextView txt_prev_breve;
-    TextView txt_allerta;
-    ImageView exp_arrow;
+    TextView     txt_time_slot;
+    TextView     txt_tmin;
+    TextView     txt_tmax;
+    TextView     txt_pressure;
+    TextView     txt_prob_prec;
+    TextView     txt_intensita_prec;
+    TextView     txt_prob_temp;
+    TextView     txt_vento_quota;
+    TextView     txt_vento_valle;
+    TextView     txt_vento_quota_dir;
+    TextView     txt_vento_valle_dir;
+    TextView     txt_zero_termico;
+    TextView     txt_prev_breve;
+    TextView     txt_allerta;
+    ImageView    exp_arrow;
     LinearLayout ll_gen;
     LinearLayout ll_prec;
     LinearLayout ll_vento;
 
     public ViewHolder(View v) {
       super(v);
-      txt_time_slot         = v.findViewById(R.id.item_weather_report_txt_fascia);
-      txt_tmin              = v.findViewById(R.id.item_weather_report_txt_tmin);
-      txt_tmax              = v.findViewById(R.id.item_weather_report_txt_tmax);
-      txt_pressure          = v.findViewById(R.id.item_weather_report_txt_pressione);
-      txt_prob_prec         = v.findViewById(R.id.item_weather_report_txt_prob_prec);
-      txt_intensita_prec    = v.findViewById(R.id.item_weather_report_txt_intensita_prec);
-      txt_prob_temp         = v.findViewById(R.id.item_weather_report_txt_prob_temp);
-      txt_vento_quota       = v.findViewById(R.id.item_weather_report_txt_vento_quota);
-      txt_vento_valle       = v.findViewById(R.id.item_weather_report_txt_vento_valle);
-      txt_vento_quota_dir   = v.findViewById(R.id.item_weather_report_txt_vento_quota_dir);
-      txt_vento_valle_dir   = v.findViewById(R.id.item_weather_report_txt_vento_valle_dir);
-      txt_zero_termico      = v.findViewById(R.id.item_weather_report_txt_zero);
-      txt_prev_breve        = v.findViewById(R.id.item_weather_report_txt_prev_breve);
-      txt_allerta           = v.findViewById(R.id.item_weather_report_allerta);
-      ll_gen                = v.findViewById(R.id.item_weather_report_l_gen);
-      ll_prec               = v.findViewById(R.id.item_weather_report_l_pioggia);
-      ll_vento              = v.findViewById(R.id.item_weather_report_l_vento);
-      exp_arrow             = v.findViewById(R.id.item_weather_report_img_arrow);
+      txt_time_slot       = v.findViewById(R.id.item_weather_report_txt_fascia);
+      txt_tmin            = v.findViewById(R.id.item_weather_report_txt_tmin);
+      txt_tmax            = v.findViewById(R.id.item_weather_report_txt_tmax);
+      txt_pressure        = v.findViewById(R.id.item_weather_report_txt_pressione);
+      txt_prob_prec       = v.findViewById(R.id.item_weather_report_txt_prob_prec);
+      txt_intensita_prec  = v.findViewById(R.id.item_weather_report_txt_intensita_prec);
+      txt_prob_temp       = v.findViewById(R.id.item_weather_report_txt_prob_temp);
+      txt_vento_quota     = v.findViewById(R.id.item_weather_report_txt_vento_quota);
+      txt_vento_valle     = v.findViewById(R.id.item_weather_report_txt_vento_valle);
+      txt_vento_quota_dir = v.findViewById(R.id.item_weather_report_txt_vento_quota_dir);
+      txt_vento_valle_dir = v.findViewById(R.id.item_weather_report_txt_vento_valle_dir);
+      txt_zero_termico    = v.findViewById(R.id.item_weather_report_txt_zero);
+      txt_prev_breve      = v.findViewById(R.id.item_weather_report_txt_prev_breve);
+      txt_allerta         = v.findViewById(R.id.item_weather_report_allerta);
+      ll_gen              = v.findViewById(R.id.item_weather_report_l_gen);
+      ll_prec             = v.findViewById(R.id.item_weather_report_l_pioggia);
+      ll_vento            = v.findViewById(R.id.item_weather_report_l_vento);
+      exp_arrow           = v.findViewById(R.id.item_weather_report_img_arrow);
     }
   }
 
@@ -95,72 +99,73 @@ public class WeatherReportAdapter extends RecyclerView.Adapter<WeatherReportAdap
   public void onBindViewHolder(ViewHolder holder, int position) {
 
   /**
+   * To collapse elements:
    * https://stackoverflow.com/questions/27203817/recyclerview-expand-collapse-items
    */
 
-    final boolean isExpanded = position==mExpandedPosition;
-    holder.exp_arrow.setVisibility(isExpanded?View.GONE:View.VISIBLE); // reverse
-    holder.txt_tmin.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_tmax.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_pressure.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_prob_prec.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_intensita_prec.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_prob_temp.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_vento_quota.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_vento_valle.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    final boolean isExpanded = position == mExpandedPosition;
+
+    holder.exp_arrow          .setVisibility(isExpanded?View.GONE:View.VISIBLE);
+    holder.txt_tmin           .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_tmax           .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_pressure       .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_prob_prec      .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_intensita_prec .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_prob_temp      .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_vento_quota    .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_vento_valle    .setVisibility(isExpanded?View.VISIBLE:View.GONE);
     holder.txt_vento_quota_dir.setVisibility(isExpanded?View.VISIBLE:View.GONE);
     holder.txt_vento_valle_dir.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_zero_termico.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.txt_prev_breve.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.ll_gen.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.ll_prec.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-    holder.ll_vento.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_zero_termico   .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.txt_prev_breve     .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.ll_gen             .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.ll_prec            .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+    holder.ll_vento           .setVisibility(isExpanded?View.VISIBLE:View.GONE);
+
     holder.itemView.setActivated(isExpanded);
 
+    WeatherForDayEntity wfd  = weather_report.getPrevisione().getGiorni().get(weather_day);
+    WeatherForSlotEntity wfs = wfd.getFasce().get(position);
 
-
-    if(weather_report.getPrevisione().getGiorni().get(weather_day).getDescIconaAllerte().isEmpty()) {
-      holder.txt_allerta.setVisibility(View.GONE); // reverse
+    if(wfd.getDescIconaAllerte().isEmpty())
+    {
+      holder.txt_allerta.setVisibility(View.GONE);
     }
     else
     {
-      holder.txt_allerta.setVisibility(View.VISIBLE); // reverse
-      holder.txt_allerta.setText(weather_report.getPrevisione().getGiorni().get(weather_day).getDescIconaAllerte());
-      String hex_color = weather_report.getPrevisione().getGiorni().get(weather_day).getColoreAllerte();
-      holder.txt_allerta.setBackgroundColor(Color.parseColor(hex_color));
+      // show allerta info
+      holder.txt_allerta.setVisibility(View.VISIBLE);
+      holder.txt_allerta.setText(wfd.getDescIconaAllerte());
 
+      String hex_color = wfd.getColoreAllerte();
+      holder.txt_allerta.setBackgroundColor(Color.parseColor(hex_color));
     }
 
-    holder.itemView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        mExpandedPosition = isExpanded ? -1:position;
-        notifyItemChanged(position);
-      }
+    holder.itemView.setOnClickListener(v -> {
+      mExpandedPosition = isExpanded ? -1 : position;
+      notifyItemChanged(position);
     });
 
-    //Set data to the individual list item
-    WeatherForSlotEntity wfs = weather_report.getPrevisione().getGiorni().get(weather_day).getFasce().get(position);
-    holder.txt_tmin.setText( "Temperatura minima: " + weather_report.getPrevisione().getGiorni().get(weather_day).gettMinGiorno() + "°");
-    holder.txt_tmax.setText("Temperatura massima: " + weather_report.getPrevisione().getGiorni().get(weather_day).gettMaxGiorno() + "°");
-    holder.txt_pressure.setText("Pressione atm.: " + open_weather_report.getPressure()+ " hPa");
-    holder.txt_prob_prec.setText("Prob. precipitazioni: " +wfs.getDescPrecProb() + " %");
-    holder.txt_intensita_prec.setText("Intensità precipitazioni: " +wfs.getDescPrecInten() + "");
-    holder.txt_prob_temp.setText("Prob. Temporali: " +wfs.getDescTempProb() + " %");
-    holder.txt_vento_quota.setText("Intensità vento in quota: " +wfs.getDescVentoIntQuota() + "");
-    holder.txt_vento_valle.setText("Intensità vento in valle: " +wfs.getDescVentoIntValle() + "");
-    holder.txt_vento_quota_dir.setText("Direzione vento in quota: " +wfs.getDescVentoDirQuota() + "");
-    holder.txt_vento_valle_dir.setText("Direzione vento in valle: " +wfs.getDescVentoDirValle() + "");
-    holder.txt_zero_termico.setText("Zero termico: " + wfs.getZeroTermico() + " m");
-    holder.txt_prev_breve.setText("Previsione: " + weather_report.getPrevisione().getGiorni().get(weather_day).getDescIcona());
 
-    holder.txt_time_slot.setText("Fascia per: "+wfs.getFasciaPer()+ " [" + wfs.getFasciaOre() + "]");
+    // set item data
+    holder.txt_tmin           .setText(String.format("%s: %s°", mContext.getResources().getString(R.string.s_weatherreportadapter_tmin), wfd.gettMinGiorno()));
+    holder.txt_tmax           .setText(String.format("%s: %s°", mContext.getResources().getString(R.string.s_weatherreportadapter_tmax), wfd.gettMaxGiorno()));
+    holder.txt_pressure       .setText(String.format("%s: %s hPa", mContext.getResources().getString(R.string.s_weatherreportadapter_pressure), open_weather_report.getPressure()));
+    holder.txt_prob_prec      .setText(String.format("%s: %s%%", mContext.getResources().getString(R.string.s_weatherreportadapter_prob_prec), wfs.getDescPrecProb()));
+    holder.txt_intensita_prec .setText(String.format("%s: %s", mContext.getResources().getString(R.string.s_weatherreportadapter_int_prec), wfs.getDescPrecInten()));
+    holder.txt_prob_temp      .setText(String.format("%s: %s%%", mContext.getResources().getString(R.string.s_weatherreportadapter_prob_temporali), wfs.getDescTempProb()));
+    holder.txt_vento_quota    .setText(String.format("%s: %s", mContext.getResources().getString(R.string.s_weatherreportadapter_dir_vento_quota), wfs.getDescVentoIntQuota()));
+    holder.txt_vento_valle    .setText(String.format("%s: %s", mContext.getResources().getString(R.string.s_weatherreportadapter_dir_vento_valle), wfs.getDescVentoIntValle()));
+    holder.txt_vento_quota_dir.setText(String.format("%s: %s" , mContext.getResources().getString(R.string.s_weatherreportadapter_int_vento_quota), wfs.getDescVentoDirQuota()));
+    holder.txt_vento_valle_dir.setText(String.format("%s: %s", mContext.getResources().getString(R.string.s_weatherreportadapter_int_vento_valle), wfs.getDescVentoDirValle()));
+    holder.txt_zero_termico   .setText(String.format("%s: %s m", mContext.getResources().getString(R.string.s_weatherreportadapter_zero_termico), wfs.getZeroTermico()));
+    holder.txt_prev_breve     .setText(String.format("%s: %s", mContext.getResources().getString(R.string.s_weatherreportadapter_previsione), wfd.getDescIcona()));
+    holder.txt_time_slot      .setText(String.format("%s: %s (%s)", mContext.getResources().getString(R.string.s_weatherreportadapter_fascia),wfs.getFasciaPer(), wfs.getFasciaOre()));
 
   }
 
   @Override
   public int getItemCount() {
-    //Return the number of items in your list
     return weather_report.getPrevisione().getGiorni().get(weather_day).getFasce().size();
   }
 
