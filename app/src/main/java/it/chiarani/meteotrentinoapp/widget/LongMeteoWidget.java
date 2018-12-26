a924903f12c3f27b7def15f5b626c069f498b31d
Fabio Chiarani <fabio@chiarani.it>
2018-12-26 15:22:42 +0100

    [UP] Add New Widget Skeleton

--- app/src/main/java/it/chiarani/meteotrentinoapp/widget/LongMeteoWidget.java
+++ app/src/main/java/it/chiarani/meteotrentinoapp/widget/LongMeteoWidget.java
@@ -1,56 +1,46 @@
 package it.chiarani.meteotrentinoapp.widget;
 
-import android.app.Application;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.arch.persistence.room.Room;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
-import java.util.List;
-
 import it.chiarani.meteotrentinoapp.R;
-import it.chiarani.meteotrentinoapp.database.AppDatabase;
-import it.chiarani.meteotrentinoapp.database.dao.WeatherReportDao;
-import it.chiarani.meteotrentinoapp.database.entity.WeatherReportEntity;
 import it.chiarani.meteotrentinoapp.helper.WeatherIconDescriptor;
 import it.chiarani.meteotrentinoapp.models.WeatherForDay;
-import it.chiarani.meteotrentinoapp.models.WeatherReport;
 import it.chiarani.meteotrentinoapp.repositories.WeatherReportRepository;
 
 /**
  * Implementation of App Widget functionality.
  */
-public class SmallMeteoWidget extends AppWidgetProvider {
+public class LongMeteoWidget extends AppWidgetProvider {
 
-    private static final String ACTION_SIMPLEAPPWIDGET = "ACTION_BROADCASTWIDGETSAMPLE";
+    private static final String ACTION_SIMPLEAPPWIDGET = "ACTION_BROADCASTWIDGETLONG";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.small_meteo_widget);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.long_meteo_widget);
 
 
-        Intent intent = new Intent(context, SmallMeteoWidget.class);
+        Intent intent = new Intent(context, LongMeteoWidget.class);
         intent.setAction(ACTION_SIMPLEAPPWIDGET);
 
         intent.putExtra("KEY_ID", appWidgetId);
 
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        views.setOnClickPendingIntent(R.id.small_meteo_widget_btn_refresh, pendingIntent);
+        //views.setOnClickPendingIntent(R.id.lon, pendingIntent);
 
-        new GetDataSmallMeteo(views, appWidgetId, appWidgetManager, context).execute();
+        new GetDataLongMeteo(views, appWidgetId, appWidgetManager, context).execute();
 
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+      //  appWidgetManager.updateAppWidget(appWidgetId, views);
 
     }
 
@@ -77,13 +67,13 @@ public class SmallMeteoWidget extends AppWidgetProvider {
         super.onReceive(context, intent);
         if (ACTION_SIMPLEAPPWIDGET.equals(intent.getAction())) {
 
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.small_meteo_widget);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.long_meteo_widget);
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            appWidgetManager.updateAppWidget(new ComponentName(context, SmallMeteoWidget.class), views);
+            appWidgetManager.updateAppWidget(new ComponentName(context, LongMeteoWidget.class), views);
 
 
             int widgetId = intent.getIntExtra("KEY_ID", -1);
-            new GetDataSmallMeteo(views, widgetId, appWidgetManager, context).execute();
+            new GetDataLongMeteo(views, widgetId, appWidgetManager, context).execute();
             Toast.makeText(context, "Aggiorno...", Toast.LENGTH_LONG).show();
         }
     }
@@ -97,14 +87,14 @@ public class SmallMeteoWidget extends AppWidgetProvider {
     }
 }
 
-class GetDataSmallMeteo extends AsyncTask<Void, Void, Void> {
+class GetDataLongMeteo extends AsyncTask<Void, Void, Void> {
 
     private RemoteViews views;
     private int WidgetID;
     private AppWidgetManager WidgetManager;
     private Context app;
 
-    public GetDataSmallMeteo(RemoteViews views, int appWidgetID, AppWidgetManager appWidgetManager, Context app){
+    public GetDataLongMeteo(RemoteViews views, int appWidgetID, AppWidgetManager appWidgetManager, Context app){
         this.views = views;
         this.WidgetID = appWidgetID;
         this.WidgetManager = appWidgetManager;
@@ -122,60 +112,45 @@ class GetDataSmallMeteo extends AsyncTask<Void, Void, Void> {
         WeatherReportRepository r = new WeatherReportRepository(app);
         r.getAll().observeForever(e -> {
             if(e.size() > 1 ){
-                String x = e.get(e.size()-1).getPrevisione().getLocalita();
-                WeatherForDay wfd = e.get(e.size()-1).getPrevisione().getGiorni().get(0);
-                views.setTextViewText(R.id.small_meteo_widget_txt_descrizione, wfd.getDescIcona());
-                views.setTextViewText(R.id.small_meteo_widget_txt_position, x);
-                views.setTextViewText(R.id.small_meteo_widget_txt_temperatura, wfd.gettMaxGiorno()+"°");
-                views.setTextViewText(R.id.small_meteo_widget_txt_temperatura_min_max, wfd.gettMinGiorno() + "° | " + wfd.gettMaxGiorno()+"°");
 
-                switch (WeatherIconDescriptor.getWeatherType(wfd.getIcona())) {
+
+                WeatherForDay wtf = e.get(e.size()-1).getPrevisione().getGiorni().get(0);
+                switch (WeatherIconDescriptor.getWeatherType(wtf.getIcona())){
                     case COPERTO:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_cloud);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_cloud_b);
                         break;
-
                     case COPERTO_CON_PIOGGIA:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_light_rain);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_light_rain_b);
                         break;
-
-                    case COPERTO_CON_PIOGGIA_ABBONDANTE:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_rain);
-
+                    case COPERTO_CON_PIOGGIA_ABBONDANTE :
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_rain_b);
                         break;
-
                     case COPERTO_CON_PIOGGIA_E_NEVE:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_snow_rain);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_snow_rain_b);
                         break;
-
                     case NEVICATA:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_snow);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_snow_b);
                         break;
-
                     case SOLE:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_sun);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_sun_b);
                         break;
-
                     case SOLEGGIATO:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud_b);
                         break;
-
                     case SOLEGGIATO_CON_PIOGGIA:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud_rain);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud_rain_b);
                         break;
-
                     case SOLEGGIATO_CON_PIOGGIA_E_NEVE:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud_rain_snow);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud_rain_snow_b);
                         break;
-
                     case TEMPORALE:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_thunderstorm);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_thunderstorm_b);
                         break;
-
                     case UNDEFINED:
-                        views.setImageViewResource(R.id.small_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud);
+                        views.setImageViewResource(R.id.long_meteo_widget_weather_img, R.drawable.ic_w_sun_cloud_b);
                         break;
                 }
-
+                
                 WidgetManager.updateAppWidget(WidgetID, views);
 
             }
@@ -184,4 +159,5 @@ class GetDataSmallMeteo extends AsyncTask<Void, Void, Void> {
 
     }
 }
+
 
